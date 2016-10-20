b7d9b0b361775946c31b8477dfdb4f9064f76849
Mark Vilrokx <mvilrokx@gmail.com>
2016-10-20 12:19:36 -0700

    Added a manifest file for the ORacle Cloud Container Service + removed ; from server.js + added .zip and .tgz to gitignore.

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
 // Build from these examples:
 //    https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
 //    http://www.smashingmagazine.com/2015/04/08/web-scraping-with-nodejs/
-var express    = require('express');
-var request    = require('request');
-var cheerio    = require('cheerio');
-var bodyParser = require('body-parser');
+var express = require('express')
+var request = require('request')
+var cheerio = require('cheerio')
+var bodyParser = require('body-parser')
 
-var app    = express();
-var port   = process.env.PORT || 8080;
-var router = express.Router();
+var app = express()
+var port = process.env.PORT || 8080
+var router = express.Router()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+router.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
-router.get('/', function(req, res) {
-  res.json({ message: 'APP is UP!' });
-});
+router.get('/', function (req, res) {
+  res.json({ message: 'APP is UP!' })
+})
 
-router.post('/scrape', function(req, res) {
-  var selected;
+router.post('/scrape', function (req, res) {
+  var selected
   request(req.body.url, function (error, response, body) {
     if (!error) {
-      var $ = cheerio.load(body);
+      var $ = cheerio.load(body)
       if (req.body.attr) {
-        selected = $(req.body.selector).attr(req.body.attr);
+        selected = $(req.body.selector).attr(req.body.attr)
       } else {
-        selected = $(req.body.selector).html();
+        selected = $(req.body.selector).html()
       }
-      res.json({ scraped: selected });
+      res.json({ scraped: selected })
     } else {
-      res.json({ error: error });
+      res.json({ error: error })
     }
-  });
-});
+  })
+})
 
-app.use('/api', router);
+app.use('/api', router)
 
-app.listen(port);
+app.listen(port)
 
-console.log('Magic happens on port ' + port);
+console.log('Magic happens on port ' + port)
