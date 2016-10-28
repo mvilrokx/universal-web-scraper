631de5e483332b3a551a29939d80cee58fc19850
Mark Vilrokx <mvilrokx@gmail.com>
2016-10-27 20:14:39 -0700

    rev'ed version

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ router.use(function (req, res, next) {
 })
 
 router.get('/', function (req, res) {
-  res.json({ message: 'Universal Web Scraper Services are UP!' })
+  res.json({ message: 'Universal Webscraper Service is UP!' })
 })
 
 router.post('/scrape', function (req, res) {
